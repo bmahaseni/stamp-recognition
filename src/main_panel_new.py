a7064f4704ac6521c8a9eeb4cf873aa993a60df4
behrooz <behrooz.mahasseni@gmail.com>
2017-01-21 16:30:51 -0800

    load_imae

--- src/main_panel_new.py
+++ src/main_panel_new.py
@@ -40,6 +40,28 @@ dataset = Dataset('./dataset', ['China', 'Japan', 'Malaysia', 'Singapore', 'Sout
 dataset.generate_date()
 
 
+class ImageDialog:
+
+    def __init__(self, parent, image_name):
+        global loaded_model
+
+
+        top = self.top = Toplevel(parent)
+	top.geometry("%dx%d%+d%+d" % (500, 400, 250, 125))
+	image = Image.open(image_name)
+	image.thumbnail((200, 200))
+        photo = ImageTk.PhotoImage(image)
+        label = Label(top, image=photo, text='', compound=LEFT)
+        label.image = photo  # keep a reference!
+        label.pack()
+        b = Button(top, text="Test", command=self.test_image)
+        b.pack(pady=5)
+
+	def test_image(self):
+		
+
+
+
 class TrainDialog:
 
     def __init__(self, parent):
@@ -113,59 +135,18 @@ def train_SVM(X_train, y_train):
 	svm_classifier.fit(X_train, y_train) 
 	s = pickle.dumps(svm_classifier)
 	return s
-class VerticalScrolledFrame(Frame):
-    """A pure Tkinter scrollable frame that actually works!
-    * Use the 'interior' attribute to place widgets inside the scrollable frame
-    * Construct and pack/place/grid normally
-    * This frame only allows vertical scrolling
-
-    """
-    def __init__(self, parent, *args, **kw):
-        Frame.__init__(self, parent, *args, **kw)            
-
-        # create a canvas object and a vertical scrollbar for scrolling it
-        vscrollbar = Scrollbar(self, orient=VERTICAL)
-        vscrollbar.pack(fill=Y, side=RIGHT, expand=TRUE)
-        canvas = Canvas(self, bd=0, highlightthickness=0,
-                        yscrollcommand=vscrollbar.set)
-        canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
-        vscrollbar.config(command=canvas.yview)
-
-        # reset the view
-        canvas.xview_moveto(0)
-        canvas.yview_moveto(0)
-        canvas.config(scrollregion='0 0 %s %s' % (100, 100))
-
-
-        # create a frame inside the canvas which will be scrolled with it
-        self.interior = interior = Frame(canvas)
-        interior_id = canvas.create_window(0, 0, window=interior,
-                                           anchor=NW)
-
-        # track changes to the canvas and frame width and sync them,
-        # also updating the scrollbar
-        def _configure_interior(event):
-            # update the scrollbars to match the size of the inner frame
-            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
-            canvas.config(scrollregion="0 0 %s %s" % size)
-            if interior.winfo_reqwidth() != canvas.winfo_width():
-                # update the canvas's width to fit the inner frame
-                canvas.config(width=interior.winfo_reqwidth())
-        interior.bind('<Configure>', _configure_interior)
-
-        def _configure_canvas(event):
-            if interior.winfo_reqwidth() != canvas.winfo_width():
-                # update the inner frame's width to fill the canvas
-                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
-        canvas.bind('<Configure>', _configure_canvas)
 
 def load_model():
     model_file = askopenfilename()
-    print model_file
-
+    global loaded_model
+    loaded_model = pickle.loads(joblib.load(model_file) )
 def open_file():
     image_file = askopenfilename()
     print image_file
+    d = ImageDialog(app.frame, image_file)
+
+    app.frame.wait_window(d.top)
+
 
 
 
@@ -208,9 +189,7 @@ def evaluate():
     model = pickle.loads(joblib.load(model_file) )
     X_test , y_test = dataset.get_testing_data_country()
     y_pred = model.predict(X_test)
-    
     cm = confusion_matrix(y_test, y_pred)
-        
     print(cm)
 def About():
     print "This is a simple example of a menu"
@@ -275,7 +254,7 @@ class StampApp(Tk):
 #global variables
 index = 0
 stamp_labels = []
-model_name = ''
+loaded_model = None
 algorithm = 0 # 0)logisitic regression 1) SVM
 #
 
